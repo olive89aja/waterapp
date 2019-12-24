b4e0048e00a24214c64d11c803f2f1dc0be7a008
olive89aja <oliviera89@gmail.com>
2019-12-23 20:59:24 -0600

    n

--- server.js
+++ server.js
@@ -1,3 +1,35 @@
+
+// //server/server.js
+// var express = require('express');
+// var router = require('./server/routes');
+// var app = express();
+// var React = require('react');
+// var ReactDOM = require('react-dom');
+// var mongoose = require('mongoose');
+
+
+// const MONGODB_URI =
+//   process.env.MONGODB_URI || "mongodb://localhost/mongoscrap";
+// mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
+
+// var db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error:"));
+// db.once("open", function() {
+//   console.log("Connected to Mongoose!");
+// });
+
+
+// app.use("/", router);
+
+// //Create localhost port
+// var port = process.env.PORT || 3000;
+
+// app.listen(port, function() {
+//   console.log("Listening on PORT " + port);
+// });
+// module.exports=app;
+
+
 //dependencies
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
@@ -34,4 +66,17 @@ var port = process.env.PORT || 3000;
 app.listen(port, function() {
   console.log("Listening on PORT " + port);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
 
