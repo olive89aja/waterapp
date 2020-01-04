01fcac32d20fd1662d569fd30915ab3cde330287
abdul shaikh <raaqibshaikh@yahoo.com>
2020-01-04 13:48:53 -0600

    routes for app/animation compnents/pages

--- client/src/components/Glogin/Glogin.js
+++ client/src/components/Glogin/Glogin.js
@@ -1,41 +1,24 @@
-import React from 'react';
+import React from "react";
+import ReactDOM from "react-dom";
+import GoogleLogin from "react-google-login";
 
-import ReactDOM from 'react-dom';
-
-import GoogleLogin from 'react-google-login';
-
-
-
-const responseGoogle = (response) => {
-
+const responseGoogle = response => {
   console.log(response);
-
-}
-
-
+};
 
 ReactDOM.render(
-
   <GoogleLogin
-
     clientId="658977310896-knrl3gka66fldh83dao2rhgbblmd4un9.apps.googleusercontent.com"
-
     render={renderProps => (
-
-      <button onClick={renderProps.onClick} disabled={renderProps.disabled}>This is my custom Google button</button>
-
+      <button onClick={renderProps.onClick} disabled={renderProps.disabled}>
+        This is my custom Google button
+      </button>
     )}
-
     buttonText="Login"
-
     onSuccess={responseGoogle}
-
     onFailure={responseGoogle}
-
-    cookiePolicy={'single_host_origin'}
-
+    cookiePolicy={"single_host_origin"}
   />,
 
-  document.getElementById('googleButton')
-
+  document.getElementById("googleButton")
 );
--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 // import Glogin from 'components/Glogin';
 // import Glogout from 'react-google-login';
 // import Animation from 'components/animation';
 // import Counter from 'components/counter';
 
-ReactDOM.render(<App />, document.getElementById('root') 
-);
-  
+ReactDOM.render(<App />, document.getElementById("root"));
+// const Index = ({ data }) => (
+//   <Glogin
+//     clientId="539150265253-mhuq4ueo94s5kvbt9bjf1d64gdq0c6m7.apps.googleusercontent.com"
+//     render={renderProps => (
+//       <button onClick={renderProps.onClick} disabled={renderProps.disabled}>
+//         Log in
+//       </button>
+//     )}
+//     buttonText="Login"
+//     cookiePolicy={"single_host_origin"}
+//   />
+// );
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
