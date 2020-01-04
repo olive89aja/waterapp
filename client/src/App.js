1c9a7eea986cc9a4195f2d0372bf0f3cb66b007c
abdul shaikh <raaqibshaikh@yahoo.com>
2020-01-04 14:06:25 -0600

    Added home route

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import About from "./components/About/About.js";
 import Main from "./components/Main/Main.js";
+import Home from "./components/Home/Home";
 import Nav from "./components/Nav";
 
 function App() {
@@ -11,7 +12,7 @@ function App() {
       <div>
         <Nav />
         <Switch>
-          <Route exact path="/" component={Main} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/Main" component={Main} />
           <Route exact path="/About" component={About} />
         </Switch>
