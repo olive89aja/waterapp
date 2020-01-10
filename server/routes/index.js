79cdc05e0ef695e5b25f6bdfb7d499c9a4c88bab
olive89aja <oliviera89@gmail.com>
2020-01-10 07:46:01 -0600

    Removed yarn

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,15 +2,14 @@ const express = require('express');
 const router = express.Router();
 const Clicks = require('../../models/clicks');
 
-
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
 router.post('/counter', (req, res, next) => {
-  var query = {author: "Olivier"};
-  var update = { $inc: { Clicks: 1}, author: "Olivier"};
+  var query = {author: "Noelle"};
+  var update = { $inc: { Clicks: 1}, author: "Noelle"};
   var options = {upsert: true, new: true};
   
   Clicks.findOneAndUpdate(query, update, options, function(error, result) {
