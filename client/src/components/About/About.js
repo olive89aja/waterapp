bf66f79575135f6038023f4be483594067ed690f
abdul shaikh <raaqibshaikh@yahoo.com>
2020-01-13 21:07:23 -0600

    Style changes: button/texts/design

--- client/src/components/About/About.js
+++ client/src/components/About/About.js
@@ -1,25 +1,37 @@
 import React from "react";
-import Animation from "../animation/index";
+import { text } from "body-parser";
+// import style from "./About.css";
 
+const textStyle = {
+  color: "white",
+  padding: "30%",
+  paddingTop: "7%",
+  textAlign: "center",
+  position: "relative",
+  fontSize: "12.9px"
+};
 class About extends React.Component {
   render() {
     return (
       <container>
-        <img src={process.env.PUBLIC_URL + "/water1.jpg"} />;
-        <Animation></Animation>
-        <p>
-          This app measures your water intake.<br></br>It has been conceived by
-          Abdul Shaikh, Stefan Vukasin and Olivier Auberger.<br></br>
-          The github repository is accessible{" "}
-          <a href="https://github.com/olive89aja/waterapp">here</a>
-        </p>
-        <p>
-          HOW TO USE THIS APP? After you confirmed your identity through Google,
-          <br></br>
-          use the Homepage button everytime you drink the equivalent of an 8
-          ounces<br></br> glass of water. If you do this 8 times a day, you are
-          well hydrated!
-        </p>
+        <div style={textStyle}>
+          {/* <img src={process.env.PUBLIC_URL + "/water1.jpg"} />; */}
+          <p>
+            Struggling to stay hydrated? Here's an app for you! This is an app
+            that will help you log your water intake.<br></br>Developed by:
+            <br></br> Abdul Shaikh, Stefan Vukasin and Olivier Auberger.
+            <br></br>
+            The github repository is accessible{" "}
+            <a href="https://github.com/olive89aja/waterapp">here</a>
+          </p>
+          <p>
+            HOW TO USE THIS APP? After you log in through Google,
+            <br></br>
+            Record everytime you drink the equivalent of an 8 ounces<br></br>{" "}
+            glass of water by clicking the button. If you do this 8 times a day,
+            you are well hydrated!
+          </p>
+        </div>
       </container>
     );
   }
--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -3,7 +3,10 @@ import React from "react";
 function Nav() {
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-default">
-      <a className="navbar-brand" href="/"></a>
+      {/* <a className="navbar-brand" href="/"></a> */}
+      <a className="navbar-brand" href="/About">
+        About
+      </a>
     </nav>
   );
 }
